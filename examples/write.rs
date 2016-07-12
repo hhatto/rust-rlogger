0dbabfed75bfabbb5254dae5241bdf39bdafea20
Hideo Hattori <hattori-h@klab.com>
2016-07-12 14:15:56 +0900

    add example

---
+++ examples/write.rs
@@ -0,0 +1,10 @@
+extern crate rlogger;
+use rlogger::rlogger::RLogger;
+
+fn main() {
+    let socket_path = "/path/to/rloggerd.sock";
+    let logger = RLogger::new(socket_path);
+    let tag = "this.is.tag";
+    let msg = "this is application log";
+    logger.write(tag, msg);
+}
